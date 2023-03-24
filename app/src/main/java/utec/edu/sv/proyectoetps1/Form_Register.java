ac47799118f79ebe742fffb58c666153003f9e16
marvin - 25-1976-2019 <2519762019@mail.utec.edu.sv>
2023-03-23 21:58:42 -0600

    modificaciones al login y a la vista register

---
+++ app/src/main/java/utec/edu/sv/proyectoetps1/Form_Register.java
@@ -0,0 +1,14 @@
+package utec.edu.sv.proyectoetps1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Form_Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_register);
+    }
+}
--- app/src/main/java/utec/edu/sv/proyectoetps1/MainActivity.java
+++ app/src/main/java/utec/edu/sv/proyectoetps1/MainActivity.java
@@ -2,7 +2,9 @@ package utec.edu.sv.proyectoetps1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void Register(View v){
+        Intent intento=new Intent(this,Form_Register.class);
+        startActivity(intento);
+    }
+
 }
